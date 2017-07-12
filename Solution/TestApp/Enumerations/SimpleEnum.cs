eb7a3aa8d8bb867a82f876fbbf4c292ef68c1f41
udragan <ujfa2001@yahoo.com>
2017-07-12 10:35:22 +0200

    TestApp - FilterableDataGrid usage example.

---
+++ Solution/TestApp/Enumerations/SimpleEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace DProject.TestApp.Enumerations
+{
+	/// <summary>
+	/// Simple enumeration for testing purposes.
+	/// </summary>
+	public enum SimpleEnum : short
+	{
+		Default = 0,
+		First = 1,
+		Second = 2,
+	}
+}
---
+++ Solution/TestApp/Models/SourceElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DProject.TestApp.Enumerations;
+
+namespace DProject.TestApp.Models
+{
+	/// <summary>
+	/// Mock model whose properties are going to be filtered against.
+	/// </summary>
+	public class SourceElement
+	{
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		public long Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Returns true if <see cref="SourceElement"/> is valid, otherwise false.
+		/// </summary>
+		public bool IsValid { get; set; }
+
+		/// <summary>
+		/// Gets or sets the simple enumeration value.
+		/// </summary>
+		public SimpleEnum SimpleEnumerationValue { get; set; }
+
+		/// <summary>
+		/// Gets or sets the link.
+		/// </summary>
+		public Uri Link { get; set; }
+	}
+}
